da112f463a363e19decad90d3b5bb1a315f6ab75
𝔻ℕ𝕠𝕣𝕕𝕚𝕔𝕍𝕫𝕝𝕒𝕟 <sthlm412@gmail.com>
2020-08-19 09:19:03 +0200

    findmylyrics was built

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div>
+      <footer className="footer">
+        <span>&copy;</span> 2020 Daviddev
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
